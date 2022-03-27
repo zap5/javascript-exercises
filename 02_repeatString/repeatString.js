46c66db26c624958873d32a0aebaac3c53a549e5
Zach Pierce <19274781+zap5@users.noreply.github.com>
2022-03-27 10:36:23 -0700

    Easy peasy

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    newString = "";
+    
+    if (num === 0) {
+        return newString;
+    }
+    else if (num < 0) {
+        return "ERROR";
+    }
+    else {
+        for (let i = 0; i < num; i++) {
+            newString += string;
+        }
+        return newString;
+    }
 };
 
 // Do not edit below this line
