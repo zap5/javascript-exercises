3384aa4388b09742336095da3cb337c03808b270
Zach Pierce <19274781+zap5@users.noreply.github.com>
2022-03-27 18:39:01 -0700

    Finally got it after I gave up trying to be fancy

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    const args = Array.prototype.slice.call(arguments, 1);
 
+    for (let i = 0; i < args.length; i++) {
+        if (array.indexOf(args[i]) !== -1) {
+            array.splice(array.indexOf(args[i]), 1);
+        }
+   }
+
+    return array;
 };
 
 // Do not edit below this line
