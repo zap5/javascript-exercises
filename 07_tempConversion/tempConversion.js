30e0f695bde0215ed075ffca7a74a8fb1fb5dbea
Zach Pierce <19274781+zap5@users.noreply.github.com>
2022-04-02 22:15:45 -0700

    done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+    let c = (temp - 32) * (5 / 9);
+    return Math.round(c * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+    let f = temp * (9 / 5) + 32;
+    return Math.round(f * 10) / 10;
 };
 
 // Do not edit below this line
