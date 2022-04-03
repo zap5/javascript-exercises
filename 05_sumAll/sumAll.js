2a9cf248e0f756cbd8889ad8d8fd10b7e28d1065
Zach Pierce <19274781+zap5@users.noreply.github.com>
2022-04-02 20:26:44 -0700

    lol

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    
+    if(!Number.isFinite(num1) || !Number.isFinite(num2)) {
+        return "ERROR";
+    }
+    else if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    
+    let smaller = Math.min(num1, num2);
+    let larger = Math.max(num1, num2); 
+    let result = 0;
 
+    for (let i = smaller; i <= larger; i++) {
+        result += i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
