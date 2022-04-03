5650e0b9789bebc7e81cc4386096e38c2618e3cf
Zach Pierce <19274781+zap5@users.noreply.github.com>
2022-04-02 22:05:58 -0700

    Easy

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(num) {
+    if (num % 100 === 0) {
+        if (num % 400 === 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else if (num % 4 === 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
